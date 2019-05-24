c4870b2a0643c111318df0822258fc33b630db6f
ekremigio04 <48143363+ekremigio04@users.noreply.github.com>
2019-05-24 19:49:45 +0800

    Add files via upload

---
+++ E3P1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int arr[9], num, smallest, largest, sum;
+    double avg;
+    
+    
+    for(int i=0; i<=9; i++)
+    {
+        cout<<"Enter number "<<i+1<<": ";
+        cin>>num;
+        arr[i]=num;
+    }
+    
+    for(int i=0; i<=9; i++)
+    {
+        sum+=arr[i];
+        if(i==0)
+        {
+            smallest=arr[i];
+            largest=arr[i];
+        }
+        else
+        {
+            if(smallest>arr[i])
+                smallest=arr[i];
+            if(largest<arr[i])
+                largest=arr[i];
+        }
+        
+    }
+    avg=(double)sum/10;
+    cout<<endl
+    <<"Smallest: "<<smallest<<endl
+    <<"Largest: "<<largest<<endl
+    <<"Total: "<<sum<<endl
+    <<"Average: "<<avg;
+    
+    cout << endl;
+    return 0;
+}
---
+++ E3P2.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	string arr[21], province="A", temp;
+	
+	for(int i=1, day=1; i<=21; i++, day++)
+	{
+		cout<<"Province "<<province<<", Day "<<day<<": ";
+		cin>>temp;
+		if(i==7){
+			day=0;
+			province="B";
+		}
+		if(i==14){
+			day=0;
+			province="C";
+		}
+		arr[i-1]=temp;
+	}
+	
+	province="A";
+	
+	cout<<endl<<"Displaying Values: "<<endl;
+	for(int i=1, day=1, temp; i<=21; i++, day++)
+	
+	{
+		cout<<"Province "<<province<<", Day "<<day<<"=";
+		cout<<arr[i-1]<<endl;
+		if(i==7){
+			day=0;
+			province="B";
+		}
+		if(i==14){
+			day=0;
+			province="C";
+		}
+	}
+return 0;
+}
